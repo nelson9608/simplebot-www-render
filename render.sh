922e6c34edfb29c1a497d30756376205d5f2a621
Reinier Romero Mir <nenirey@gmail.com>
2023-02-12 10:30:04 -0500

    add simplebot_render plugin

--- render.sh
+++ render.sh
@@ -14,6 +14,9 @@ python3 ./restore_keys.py
 python3 -c "import requests; r=requests.get('https://github.com/adbenitez/simplebot-scripts/raw/master/scripts/web_compress.py'); open('web_compress.py', 'wb').write(r.content)"
 python3 -m simplebot -a "$ADDR" plugin --add ./web_compress.py
 
+# add simple web server for service healthy 
+python3 -m simplebot -a "$ADDR" plugin --add ./simplebot_render.py
+
 # add the web_search plugin
 python3 -c "import requests; r=requests.get('https://github.com/adbenitez/simplebot-scripts/raw/master/scripts/web_search.py'); open('web_search.py', 'wb').write(r.content)"
 python3 -m simplebot -a "$ADDR" plugin --add ./web_search.py
