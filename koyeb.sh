b2e7ec04a1a0ddd1bba535f7533ff1ca9a823e70
Reinier Romero Mir <nenirey@gmail.com>
2023-02-26 10:01:14 -0500

    add web2image and web2pdf plugin

--- koyeb.sh
+++ koyeb.sh
@@ -14,6 +14,14 @@ python3 -m simplebot -a "$ADDR" db -s "simplebot_translator/filter_enabled" "no"
 python3 -c "import requests; r=requests.get('https://github.com/adbenitez/simplebot-scripts/raw/master/scripts/web_compress.py'); open('web_compress.py', 'wb').write(r.content)"
 python3 -m simplebot -a "$ADDR" plugin --add ./web_compress.py
 
+# add the web2pdf plugin
+python3 -c "import requests; r=requests.get('https://github.com/adbenitez/simplebot-scripts/raw/master/scripts/web2pdf.py'); open('web2pdf.py', 'wb').write(r.content)"
+python3 -m simplebot -a "$ADDR" plugin --add ./web2pdf.py
+
+# add the web_screenshoot plugin
+python3 -c "import requests; r=requests.get('https://github.com/adbenitez/simplebot-scripts/raw/master/scripts/web_screenshoot.py'); open('web_screenshoot.py', 'wb').write(r.content)"
+python3 -m simplebot -a "$ADDR" plugin --add ./web_screenshoot.py
+
 # add simple web server for service healthy 
 python3 -m simplebot -a "$ADDR" plugin --add ./simplebot_render.py
 
