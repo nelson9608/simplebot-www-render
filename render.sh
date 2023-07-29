3fb5b16a30ab3c52efda362225f80a37bf5f8804
Nelson <104731311+nelson9608@users.noreply.github.com>
2023-07-29 17:55:27 -0400

    Update render.sh

--- render.sh
+++ render.sh
@@ -2,7 +2,7 @@
 
 # configure the bot
 python3 -m simplebot init "$ADDR" "$PASSWORD"
-python3 -m simplebot -a "$ADDR" set_name "$BOT_NAME"
+python3 -m simplebot -a "$ADDR" set_name "$BOTNAME"
 python3 -m simplebot -a "$ADDR" db -s "simplebot_instantview/max_size" "15728640"  # 15MB
 python3 -m simplebot -a "$ADDR" db -s "simplebot_downloader/mode" "command"
 python3 -m simplebot -a "$ADDR" db -s "simplebot_downloader/max_size" "1073741824"  # 1GB
@@ -24,6 +24,14 @@ python3 -m simplebot -a "$ADDR" plugin --add ./web_search.py
 # add the youtube plugin
 python3 -c "import requests; r=requests.get('https://github.com/adbenitez/simplebot-scripts/raw/master/scripts/youtube.py'); open('youtube.py', 'wb').write(r.content)"
 python3 -m simplebot -a "$ADDR" plugin --add ./youtube.py
+
+# add the web2pdf plugin
+python3 -c "import requests; r=requests.get('https://github.com/adbenitez/simplebot-scripts/raw/master/scripts/web2pdf.py'); open('web2pdf.py', 'wb').write(r.content)"
+python3 -m simplebot -a "$ADDR" plugin --add ./web2pdf.py
+
+# add the Web screenshot plugin
+python3 -c "import requests; r=requests.get('https://github.com/adbenitez/simplebot-scripts/raw/master/scripts/web_screenshot.py'); open('web_screenshot.py', 'wb').write(r.content)"
+python3 -m simplebot -a "$ADDR" plugin --add ./web_screenshot.py
 
 # add the encryption_error plugin to leverage key changes
 python3 -c "import requests; r=requests.get('https://github.com/adbenitez/simplebot-scripts/raw/master/scripts/encryption_error.py'); open('encryption_error.py', 'wb').write(r.content)"
